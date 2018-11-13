d76b3d3214898050c3c2011ebbffba94d18b7d23
xxxxx <991986182@qq.com>
2018-11-13 17:57:38 +0800

    commom

---
+++ js/constant/constant.js
@@ -0,0 +1,13 @@
+angular.module('myApp')
+    .constant('jobType',JSON.parse(sessionStorage.getItem('jobType')))
+    .constant('hotSearch',JSON.parse(sessionStorage.getItem('hotSearch')))
+    .constant('arrival',JSON.parse(sessionStorage.getItem('arrival')))
+    .constant('boon',JSON.parse(sessionStorage.getItem('boon')))
+    .constant('expList',JSON.parse(sessionStorage.getItem('expList')))
+    .constant('eduList',JSON.parse(sessionStorage.getItem('eduList')))
+    .constant('bannerImg',JSON.parse(sessionStorage.getItem('bannerImg')))
+    .constant('orderBy',[
+        {name:"正序",value:0},
+        {name:"倒序",value:1},
+        ])
+;
